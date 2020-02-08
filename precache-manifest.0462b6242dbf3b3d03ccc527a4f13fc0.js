807f0f55a6135c7041ccd43bfbcc3280ca4d3d15
shamadeaus <shamadeaus@gmail.com>
2020-02-08 16:11:26 -0600

    Updates

---
+++ precache-manifest.0462b6242dbf3b3d03ccc527a4f13fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c741495f8c3e3d604e8adae3b160c46",
+    "url": "/poke-pages/index.html"
+  },
+  {
+    "revision": "cce94a1a5f12d09337ab",
+    "url": "/poke-pages/static/js/2.8e6983d5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/poke-pages/static/js/2.8e6983d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65c68db5299743726df",
+    "url": "/poke-pages/static/js/main.48728b7c.chunk.js"
+  },
+  {
+    "revision": "d0e6cf2d38a49c2f5959",
+    "url": "/poke-pages/static/js/runtime-main.aeb9200a.js"
+  },
+  {
+    "revision": "aee5eebe7d1b2582f81f1d5dfcff0f2a",
+    "url": "/poke-pages/static/media/titleimg.aee5eebe.png"
+  }
+]);
